14e3b914caae60ef1c2afa882cde74dd63bcb907
alekseiagafonov <aleksei.agafonov.2605@gmail.com>
2022-10-13 23:56:07 +0400

    Home && Details

---
+++ screens/Details.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+const Details = () => (
+  <View>
+    <Text>Hi Details</Text>
+  </View>
+);
+
+export default Details;
---
+++ screens/Home.js
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+const Home = () => (
+  <View>
+    <Text>Hi</Text>
+  </View>
+);
+
+export default Home;
