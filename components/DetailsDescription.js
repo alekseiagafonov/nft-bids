cdab7f93822e5a10ff4d1c2fbf7147ff38c151a3
alekseiagafonov <aleksei.agafonov.2605@gmail.com>
2022-10-14 00:43:35 +0400

    DetailsDescription

---
+++ components/DetailsDescription.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+
+const DetailsDescription = ({ data }) => {
+  const [text, setText] = useState(data.description.slice(0, 100));
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <>
+      <View
+        style={{
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      />
+
+      <View
+        style={{
+          marginVertical: 1,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: "bold",
+            color: "red",
+          }}
+        >
+          Description
+        </Text>
+        <View
+          style={{
+            marginTop: 3,
+          }}
+        >
+          <Text
+            style={{
+              color: "red",
+            }}
+          >
+            {text}
+            {!readMore && "..."}
+            <Text
+              style={{
+                color: "red",
+              }}
+              onPress={() => {
+                if (!readMore) {
+                  setText(data.description);
+                  setReadMore(true);
+                } else {
+                  setText(data.description.slice(0, 100));
+                  setReadMore(false);
+                }
+              }}
+            >
+              {readMore ? " Show Less" : " Read More"}
+            </Text>
+          </Text>
+        </View>
+      </View>
+    </>
+  );
+};
